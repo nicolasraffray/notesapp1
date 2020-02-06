c37cc8b21a1b038bc4270c786a5b18541c1dd683
nicolasraffray <nraffray@gmail.com>
2020-02-06 17:03:49 +0000

    refactor of map to for loop

--- spec/note-list-model-test.js
+++ spec/note-list-model-test.js
@@ -10,3 +10,11 @@
   assert.isTrue(noteList.returnNotes().length === 2);
   assert.isTrue(noteList.returnNotes()[0].getText() === 'Hello');
 })();
+
+(function testNoteID() {
+  var notelist = new NoteList();
+  notelist.createNote("Hello")
+  notelist.createNote("Howdy")
+  assert.isTrue(notelist.returnNotes()[0].id === 0 )
+  assert.isTrue(notelist.returnNotes()[1].id === 1 )
+})()
--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,24 +1,20 @@
 
 (function viewNote() {
+
   var notelist = new NoteList
   notelist.createNote("Hello")
   notelist.createNote("Hi")
   var viewnote = new ViewNotes(notelist)
   assert.isTrue(viewnote.shownotes() === "<ul><li><div>Hello</div></li><li><div>Hi</div></li></ul>");
+
 })();
 
 
 (function testCharCap() {
-
-var notelist = new NoteList
+  var notelist = new NoteList
   notelist.createNote("1234567901234567890hello")
   var viewnote = new ViewNotes(notelist)
-  console.log(viewnote.shownotes())
+  console.log(viewnote)
   assert.isTrue(viewnote.shownotes() === "<ul><li><div>1234567901234567890</div></li></ul>")
   
-
-
-
-
-
 })();
