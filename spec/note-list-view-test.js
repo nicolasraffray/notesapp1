f687770767c2d4b7b0ff34d7f9acf40f92fa07bd
Jack Willee <jackwillee1995@gmail.com>
2020-02-06 15:48:52 +0000

    put a limit of 20 max characters

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -6,3 +6,19 @@
   var viewnote = new ViewNotes(notelist)
   assert.isTrue(viewnote.shownotes() === "<ul><li><div>Hello</div></li><li><div>Hi</div></li></ul>");
 })();
+
+
+(function testCharCap() {
+
+var notelist = new NoteList
+  notelist.createNote("1234567901234567890hello")
+  var viewnote = new ViewNotes(notelist)
+  console.log(viewnote.shownotes())
+  assert.isTrue(viewnote.shownotes() === "<ul><li><div>1234567901234567890</div></li></ul>")
+  
+
+
+
+
+
+})();
